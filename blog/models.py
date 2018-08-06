329fbb3d560455161d1fa65aefbf87a28424e329
Aleksandr-v <mixft@mail.ru>
2018-08-06 22:25:47 +1000

    Added views to create/edit blog post inside the site.

--- blog/models.py
+++ blog/models.py
@@ -17,3 +17,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.description
